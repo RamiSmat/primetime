fc313cbc099aee879d15b71434b558e0b4aa5f92
rami <rami.smat@vonoy.co>
2026-09-21 10:30:55 +0100

    feat(scheduler): compute the next primer run from a schedule

--- packages/scheduler/src/index.ts
+++ packages/scheduler/src/index.ts
@@ -1,2 +1,10 @@
-// Scheduling behavior will be introduced with its own tested requirements.
-export {};
+export type { ScheduleConfig, Weekday } from "./types.js";
+export { WEEKDAYS } from "./types.js";
+
+export type { WorkStartTime } from "./config.js";
+export { InvalidScheduleConfigError, parseScheduleConfig, parseWorkStartTime } from "./config.js";
+
+export { computeNextPrimerRun, NoActivePrimerWindowError } from "./schedule.js";
+
+export type { ZonedCalendarDate, ZonedTimeParts } from "./timezone.js";
+export { addCalendarDays, getZonedCalendarDate, getZonedWeekday, zonedTimeToUtc } from "./timezone.js";
